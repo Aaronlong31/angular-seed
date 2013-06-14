320f641fe3d6fc08ffbb66e3354f6fae2751b031
Aaronlong31 <aaron198809@gmail.com>
2013-06-14 16:09:27 +0800

    update

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,6 +6,9 @@ function DocListCtrl($scope, $http) {
 	$http.get('js/_doc.json').success(function(data) {
 	    $scope.docs = data;
 	  });
+	$scope.queryKey = function (key) {
+		$scope.query = key;
+	}
 }
 function DocDetailCtrl($scope){
 	
--- app/js/filters.js
+++ app/js/filters.js
@@ -1,3 +1,28 @@
 'use strict';
 
 /* Filters */
+angular.module('docFilters', []).filter('methodStyle', function(){
+	return function(input){
+		if (input == "GET") {
+			return "btn-success";
+		} else if (input == "POST") {
+			return "btn-primary";
+		} else if (input == "DELETE") {
+			return "btn-danger";
+		} else if (input == "PUT") {
+			return "btn-warning";
+		} else {
+			return "";
+		}
+	}
+}).filter('splitPath', function() {
+	return function(input, scope) {
+        var ss = input.replace(/\[|\]|\s+/g, '').split('/');
+        var pathHtml = "";
+        $.each(ss,function(){
+            if(this!='')
+                pathHtml += "/<a href='javascript:void(0);' class='key'>" + this + "</a>";
+        });
+        return pathHtml;
+	}
+});
