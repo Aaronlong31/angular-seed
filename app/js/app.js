f498e455b6330c92a8145e838307cc0e00a5046a
Aaronlong31 <aaron198809@gmail.com>
2013-06-14 16:08:30 +0800

    update

--- app/js/app.js
+++ app/js/app.js
@@ -3,10 +3,11 @@
 
 // Declare app level module which depends on filters, and services
 /*
-angular.module('docapp').
+angular.module('liba-doc-app', ['docFilters']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/docs', {templateUrl: 'partials/list.html', controller: DocListCtrl});
     $routeProvider.when('/:docId', {templateUrl: 'partials/detail.html', controller: DocDetailCtrl});
     $routeProvider.otherwise({redirectTo: '/docs'});
   }]);
-*/
+  */
+angular.module('liba-doc-app',['docFilters']);
